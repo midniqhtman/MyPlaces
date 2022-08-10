7bcba5081360ecb74cb3c4fe27462fd0fd5956fe
midniqhtman <c1esi@mail.ru>
2022-08-11 00:54:42 +0300

    initial

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+    let restarauntNames = [
+    "Coffetown", "Steakhouse", "CFC", "Foodinbox", "Samburger",
+    "Kannamchicken", "Chickenhut", "Tbiliso", "Drinks"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    //MARK: TableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restarauntNames.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = restarauntNames[indexPath.row]
+        cell.imageView?.image = UIImage(systemName: "lasso")
+        cell.imageView?.layer.cornerRadius =  cell.frame.size.height / 2
+        
+        cell.imageView?.clipsToBounds = true
+    
+        return cell
+    }
+    
+    //MARK: TableViewDelegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
 }
 

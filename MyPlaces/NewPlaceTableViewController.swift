09c58a6ebb584c93e9d3f60132d1de9c19d0275b
midniqhtman <c1esi@mail.ru>
2022-08-21 18:42:05 +0300

    first commit

---
+++ MyPlaces/NewPlaceTableViewController.swift
@@ -0,0 +1,168 @@
+//
+//  NewPlaceTableViewController.swift
+//  MyPlaces
+//
+//  Created by Байсаев Зубайр on 11.08.2022.
+//
+
+import UIKit
+
+class NewPlaceTableViewController: UITableViewController, UINavigationControllerDelegate {
+    
+    var imageIsChanged = false
+    var currentPlace: Place?
+    
+    @IBOutlet weak var placeImage: UIImageView!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var placeLocation: UITextField!
+    @IBOutlet weak var placeType: UITextField!
+    @IBOutlet weak var placeName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        tableView.tableFooterView = UIView()
+        saveButton.isEnabled = false
+        placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        setupEditinScreen()
+    }
+
+   //MARK: Table View Delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            
+            let actionSheet = UIAlertController(title: nil,
+                                                message: nil,
+                                                preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
+        } else {
+            view.endEditing(true)
+        }
+    }
+    
+    func savePlace() {
+        
+        var image: UIImage?
+        
+        if imageIsChanged {
+            image = placeImage.image
+        } else {
+            image = #imageLiteral(resourceName: "imagePlaceholder")
+        }
+        
+        let imageData = image?.pngData()
+        
+        let newPlace = Place(name: placeName.text!, location: placeLocation.text, type: placeType.text, imageData: imageData)
+        
+        if currentPlace != nil {
+            try! realm.write {
+                currentPlace?.name = newPlace.name
+                currentPlace?.location = newPlace.location
+                currentPlace?.type = newPlace.type
+                currentPlace?.imageData = newPlace.imageData
+            }
+        } else {
+                StorageManager.saveObject(newPlace)
+            }
+        }
+        
+    
+    private func setupEditinScreen() {
+        if currentPlace != nil {
+            
+            setupNavigationBar()
+            imageIsChanged = true 
+            
+            guard let data = currentPlace?.imageData, let image = UIImage(data: data) else { return }
+            
+            if let topItem = navigationController?.navigationBar.topItem {
+                topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+            }
+            
+            placeImage.image = image
+            placeImage.contentMode = .scaleAspectFill
+            placeName.text = currentPlace?.name
+            placeLocation.text = currentPlace?.location
+            placeType.text = currentPlace?.type
+        }
+    }
+    
+    private func setupNavigationBar() {
+        
+        navigationItem.leftBarButtonItem = nil
+        title = currentPlace?.name
+        saveButton.isEnabled = true
+    }
+    
+    @IBAction func cancelAction(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+}
+
+//MARK: Text Field Delegate
+
+extension NewPlaceTableViewController: UITextFieldDelegate {
+    
+    //Hiding the keyboard
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc private func textFieldChanged() {
+        
+        if placeName.text?.isEmpty == false {
+            saveButton.isEnabled = true
+        } else {
+            saveButton.isEnabled = false
+        }
+    }
+}
+
+
+//MARK: Work with image
+
+extension NewPlaceTableViewController: UIImagePickerControllerDelegate {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        placeImage.image = info[.editedImage] as? UIImage
+        placeImage.contentMode = .scaleAspectFill
+        placeImage.clipsToBounds = true
+        imageIsChanged = true
+        dismiss(animated: true)
+    }
+}
--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -6,38 +6,148 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   
-    let restarauntNames = [
-    "Coffetown", "Steakhouse", "CFC", "Foodinbox", "Samburger",
-    "Kannamchicken", "Chickenhut", "Tbiliso", "Drinks"
-    ]
-
-    override func viewDidLoad() {
+    private var places: Results<Place>!
+    private var filtredPlaces: Results<Place>!
+    private var ascendingSorting = true
+    private var seaarchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        return searchController.isActive && !seaarchBarIsEmpty
+    }
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentedControle: UISegmentedControl!
+    @IBAction func sortSelection(_ sender: UISegmentedControl) {
+        sorting()
+    }
+    @IBOutlet weak var reversedSortingButton: UIBarButtonItem!
+    @IBAction func reversedSorting(_ sender: Any) {
+        
+        ascendingSorting.toggle()
+        
+        if ascendingSorting == true {
+            reversedSortingButton.image = #imageLiteral(resourceName: "AZ")
+        } else {
+            reversedSortingButton.image = #imageLiteral(resourceName: "ZA")
+        }
+        
+        sorting()
+    }
+    
+    private func sorting() {
+        
+        if segmentedControle.selectedSegmentIndex == 0 {
+            places = places.sorted(byKeyPath: "date", ascending: ascendingSorting)
+        } else {
+            places = places.sorted(byKeyPath: "name", ascending: ascendingSorting)
+        }
+        
+        tableView.reloadData()
+    }
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
+        
+        places = realm.objects(Place.self)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    
+        //MARK: SetupSearchController
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
     }
 
     //MARK: TableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restarauntNames.count
+        if isFiltering {
+            return filtredPlaces.count
+        }
+        return places.isEmpty ? 0 : places.count
     }
-    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        cell.nameLabel?.text = restarauntNames[indexPath.row]
-        cell.imageOfPlace?.image = UIImage(systemName: "lasso")
+        var place = Place()
+
+        if isFiltering {
+            place = filtredPlaces[indexPath.row]
+        } else {
+            place = places[indexPath.row]
+        }
+        
+        cell.nameLabel?.text = place.name
+        cell.locationLabel?.text = place.location
+        cell.typeLabel?.text = place.type
+        cell.imageOfPlace.image = UIImage(data: place.imageData!)
+      
         cell.imageOfPlace?.layer.cornerRadius =  cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace?.clipsToBounds = true
     
         return cell
+    
     }
     
     //MARK: TableViewDelegate
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let place = places[indexPath.row]
+        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (_, _) in
+            
+            StorageManager.deleteObject(place)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        return [deleteAction]
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            let place: Place
+            if isFiltering {
+                place = filtredPlaces[indexPath.row]
+            } else {
+                place = places[indexPath.row]
+            }
+            
+            let nPTVC = segue.destination as! NewPlaceTableViewController
+            nPTVC.currentPlace = place
+        }
+    }
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        
+        guard let newPlaceVc = segue.source as? NewPlaceTableViewController else { return }
+        
+        newPlaceVc.savePlace()
+        tableView.reloadData()
     }
 }
 
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    private func filterContentForSearchText(_ searchText: String) {
+        
+        filtredPlaces = places.filter("name CONTAINS[c] %@ OR location CONTAINS[c] %@", searchText, searchText )
+        
+        tableView.reloadData()
+    }
+    
+    
+}
